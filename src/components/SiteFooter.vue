f8d2222fbce32510340e8a46c5ded8e6627b77bf
ashlee-seagle <ashlee-seagle10@gmail.com>
2026-06-21 16:35:18 -0400

    Create global layout shell

---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="border-t border-site-border/60">
+        <div class="mx-auto max-w-7xl px-4 py-8 text-sm text-site-muted sm:px-6 lg:px-8">
+          © 2026 Ashlee Seagle. All rights reserved.
+        </div>
+      </footer>
+</template>
