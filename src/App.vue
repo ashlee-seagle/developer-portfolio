e2b3ba40c9c9a703787b57fafa3ffc7c8555018d
ashlee-seagle <ashlee-seagle10@gmail.com>
2026-06-24 08:41:27 -0400

    feat: add contact section

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import MainLayout from './layouts/MainLayout.vue';
 import AboutSection from './sections/AboutSection.vue';
+import ContactSection from './sections/ContactSection.vue';
 import HeroSection from './sections/HeroSection.vue';
 import ProjectSection from './sections/ProjectSection.vue';
 import SkillsSection from './sections/SkillsSection.vue';
@@ -12,5 +13,6 @@ import SkillsSection from './sections/SkillsSection.vue';
       <ProjectSection />
       <AboutSection />
       <SkillsSection />
+      <ContactSection />
   </MainLayout>
 </template>
